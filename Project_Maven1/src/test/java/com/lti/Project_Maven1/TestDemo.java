2fa02211ee1748c9874b950f3b32df1c81f1b75d
priya <priyankapatil3182000@gmail.com>
2022-09-10 17:32:57 +0530

    commit1

---
+++ Project_Maven1/src/test/java/com/lti/Project_Maven1/TestDemo.java
@@ -0,0 +1,20 @@
+package com.lti.Project_Maven1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestDemo {
+	
+	@Test
+	
+	void strChcek()
+	{
+		String s1="Demo";
+		
+		System.out.println("This is test case for string chcek");
+		assertEquals("Demo",s1);
+	}
+
+
+}
